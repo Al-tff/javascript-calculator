5502ffeeace010a3a8ef8297edbb04790eb0fec4
Al-tff <althafmalthaf2803@gmail.com>
2024-07-07 23:08:05 +0530

    main.js

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-function buttonclick(val){
-    document.getElementById("screen").value+=val;
-
+function buttonclick(val) {
+    document.getElementById("screen").value += val;
 }
-function allClear(){
-   document.getElementById("screen").value="";
+function allClear() {
+   document.getElementById("screen").value = "";
 }
 function lastClear(){
-   // document.getElementById("screen").value.slice(0,-1);
+    let result = document.getElementById("screen");
+    result.value = result.value.slice(0, -1);
 }
-
-function equalClick(){
-    var text=document.getElementById("screen").value;
-    var result=eval(text);
-    document.getElementById("screen").value=result;
+function equalClick() {
+    let text = document.getElementById("screen").value;
+    let result = eval(text);
+    document.getElementById("screen").value = result;
 }
